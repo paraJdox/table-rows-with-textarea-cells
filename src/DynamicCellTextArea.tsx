44d9e74b5a013621310d1affa63e2d524199b391
John Dave Dalmao <johndavedalmao@gmail.com>
2023-09-17 17:54:16 +0800

    used MutationObserver instead of ResizeObserver to have all child td elements share the same height style (working solution, with performance issues)

--- src/DynamicCellTextArea.tsx
+++ src/DynamicCellTextArea.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef } from "react";
 
 export type TableRowCell = {
   id: number;
@@ -6,46 +6,43 @@ export type TableRowCell = {
   text: string;
 };
 
-const DynamicCellTextArea = () => {
+type DynamicCellTextAreaProps = {
+  currentRow: TableRowCell;
+  height: string;
+  setHeight: Dispatch<SetStateAction<string>>;
+};
+
+const DynamicCellTextArea = (props: DynamicCellTextAreaProps) => {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const textareaWrapperRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (textareaWrapperRef.current === null || textareaRef.current === null) {
+    if (textareaRef.current === null) {
       return;
     }
 
-    const textareaElementObserver = new ResizeObserver(() => {
-      if (textareaRef.current === null || textareaWrapperRef.current === null) {
-        return;
-      }
+    textareaRef.current.style.minWidth = "100%";
+    textareaRef.current.style.minHeight = "100%";
+    textareaRef.current.style.height = props.height;
 
-      if (
-        textareaWrapperRef.current.getBoundingClientRect().width >
-        textareaRef.current.getBoundingClientRect().width
-      ) {
-        textareaRef.current.style.minWidth = "100%";
-      }
-
-      if (
-        textareaWrapperRef.current.getBoundingClientRect().height >
-        textareaRef.current.getBoundingClientRect().height
-      ) {
-        textareaRef.current.style.minHeight = "100%";
-      }
+    const mutationObserver = new MutationObserver(() => {
+      // set the height here so that child components (on the same row)
+      // can have a shared height style (on state)
+      props.setHeight(textareaRef.current?.style.height as string);
     });
 
-    textareaElementObserver.observe(textareaRef.current);
-    textareaElementObserver.observe(textareaWrapperRef.current);
-  }, []);
+    mutationObserver.observe(textareaRef.current, {
+      attributes: true,
+      attributeFilter: ["style"],
+    });
+  }, [props]);
 
   return (
-    <td className="border border-red-400 h-px overflow">
-      <div ref={textareaWrapperRef} className="flex w-full h-full">
+    <td className="h-px border border-red-400 overflow">
+      <div className="flex w-full h-full">
         <textarea
           ref={textareaRef}
           rows={1}
-          className="w-full resize bg-red-500 py-2 px-3"
+          className="w-full px-3 py-2 bg-red-500 resize"
         />
       </div>
     </td>
--- src/TableRow.tsx
+++ src/TableRow.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import DynamicCellTextArea, { TableRowCell } from "./DynamicCellTextArea";
 
 type TableRow = {
@@ -10,10 +11,17 @@ type TableRowProps = {
 };
 
 const TableRow = (props: TableRowProps) => {
+  const [textAreaHeight, setTextAreaHeight] = useState<string>("100%");
+
   return (
     <tr>
       {props.currentRow.cells.map((rowValue) => (
-        <DynamicCellTextArea key={rowValue.id} />
+        <DynamicCellTextArea
+          key={rowValue.id}
+          currentRow={rowValue}
+          height={textAreaHeight}
+          setHeight={setTextAreaHeight}
+        />
       ))}
     </tr>
   );
